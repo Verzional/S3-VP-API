a252d95ccc8f4f31cda54ebb21f1b744fa63f20e
Verzional <verzional777@gmail.com>
2024-11-21 13:00:24 +0700

    Model Fixes

--- src/model/mahasiswaModel.ts
+++ src/model/mahasiswaModel.ts
@@ -1,15 +1,23 @@
 import {Mahasiswa} from "@prisma/client"
 
-export interface MahasiswaReqRes {
-    id? : number
+export interface MahasiswaRequest {
     nama: string
     nim: string
+    mataKuliahId: number
 }
 
-export function toMahasiswaResponse(mahasiswa: Mahasiswa): MahasiswaReqRes {
+export interface MahasiswaResponse {
+    id: number
+    nama: string
+    nim: string
+    mataKuliahId: number
+}
+
+export function toMahasiswaResponse(mahasiswa: Mahasiswa): MahasiswaResponse {
     return {
         id: mahasiswa.id,
         nama: mahasiswa.nama,
-        nim: mahasiswa.nim
+        nim: mahasiswa.nim,
+        mataKuliahId: mahasiswa.mataKuliahId
     }
 }
--- src/model/mataKuliahModel.ts
+++ src/model/mataKuliahModel.ts
@@ -1,19 +1,25 @@
 import { MataKuliah } from "@prisma/client";
 
-export interface MataKuliahReqRes {
-    id? : number
+export interface MataKuliahRequest {
     nama: string
     kodeUnik: string
     namaDosen: string
     linkUrlImage: string
 }
 
-export function toMataKuliahResponse(mataKuliah: MataKuliah): MataKuliahReqRes {
-    return {
-        id: mataKuliah.id,
-        nama: mataKuliah.nama,
-        kodeUnik: mataKuliah.kodeUnik,
-        namaDosen: mataKuliah.namaDosen,
-        linkUrlImage: mataKuliah.linkUrlImage
-    }
+export interface MataKuliahResponse {
+    id: number
+    nama: string
+    kodeUnik: string
+    namaDosen: string
+    linkUrlImage: string
 }
+
+// export function toMataKuliahResponse(mataKuliah: MataKuliah): MataKuliahReqRes {
+//     return {
+//         nama: mataKuliah.nama,
+//         kodeUnik: mataKuliah.kodeUnik,
+//         namaDosen: mataKuliah.namaDosen,
+//         linkUrlImage: mataKuliah.linkUrlImage
+//     }
+// }
